6ba97a37c09bff9db55a6f8680b39a07698015d3
Mahdi <mahdihamldar@gmail.com>
2023-01-08 10:05:43 +0330

    Socket.io - server-client communication

--- index.ts
+++ index.ts
@@ -5,8 +5,15 @@ import localize from './src/init/localize';
 import db from './src/init/db';
 import theApp from './src/init/theApp';
 
-dotenv.config();
 const app: Express = express();
+
+const server = app.listen(8081, () => {
+  console.log(`⚡️ [server]: Server is running at https://localhost:${8081}`);
+});
+
+const io = require('socket.io')(server)
+
+dotenv.config();
 
 const cors = require('cors');
 const corsOptions = {
@@ -19,7 +26,28 @@ localize(app);
 db();
 routes(app);
 
-app.listen(8081, () => {
-  console.log(`⚡️ [server]: Server is running at https://localhost:${8081}`);
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+//The 'connection' is a reserved event name in socket.io
+//For whenever a connection is established between the server and a client
+io.on('connection', (socket) => {
+
+	//Displaying a message on the terminal
+    console.log('a user connected');
+    //Sending a message to the client
+    socket.emit('serverToClient', "Hello, client!");
+    //Receiving a message from the client and putting it on the terminal
+    socket.on('clientToServer', data => {
+        console.log(data);
+    })
+    //When the client sends a message via the 'clientToClient' event
+    //The server forwards it to all the other clients that are connected
+    socket.on('clientToClient', data => {
+        socket.broadcast.emit('serverToClient', data);
+    })
+    
 });
+
 export default app;
